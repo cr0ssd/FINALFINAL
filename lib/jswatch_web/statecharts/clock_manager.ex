4314dbb71681a70a1332adb82e985b13a8938f12
iii <isaacmartinez@MacBook-Pro-de-Isaac-2.local>
2025-06-13 12:02:55 -0600

    Ignora milisegundos

--- lib/jswatch_web/statecharts/clock_manager.ex
+++ lib/jswatch_web/statecharts/clock_manager.ex
@@ -21,12 +21,13 @@ defmodule JswatchWeb.ClockManager do
   def handle_info(:working_working, %{ui_pid: ui, time: time, alarm: alarm, st: Working} = state) do
     Process.send_after(self(), :working_working, 1000)
     time = Time.add(time, 1)
-    if time == alarm do
+    #Ahora ignora milisegundos
+    if Time.truncate(time, :second) == Time.truncate(alarm, :second) do
       IO.puts("ALARM!!!")
       :gproc.send({:p, :l, :ui_event}, :start_alarm)
     end
-    GenServer.cast(ui, {:set_time_display, Time.truncate(time, :second) |> Time.to_string })
-    {:noreply, state |> Map.put(:time, time) }
+    GenServer.cast(ui, {:set_time_display, Time.truncate(time, :second) |> Time.to_string()})
+    {:noreply, state |> Map.put(:time, time)}
   end
 
   def handle_info(_event, state), do: {:noreply, state}
