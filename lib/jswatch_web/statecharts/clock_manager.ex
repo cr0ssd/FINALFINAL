be71473d458d5b8465fd841ea19faba104d40919
iii <isaacmartinez@MacBook-Pro-de-Isaac-2.local>
2025-06-13 11:55:22 -0600

    cambio 5 a 10 segundos en clockmanager

--- lib/jswatch_web/statecharts/clock_manager.ex
+++ lib/jswatch_web/statecharts/clock_manager.ex
@@ -10,11 +10,12 @@ defmodule JswatchWeb.ClockManager do
     {:ok, %{ui_pid: ui, time: time, alarm: alarm, st: Working}}
   end
 
+  # Se cambia de 5 a 10 segundos.
   def handle_info(:update_alarm, state) do
-    {_, now} = :calendar.local_time()
-    time = Time.from_erl!(now)
-    alarm = Time.add(time, 5)
-    {:noreply, %{state | alarm: alarm}}
+    IO.puts("ClockManager: Recibido update_alarm. Recalculando alarma...")
+    # Ahora usa el tiempo actual del estado para calcular la nueva alarma.
+    new_alarm_time = Time.add(state.time, 10)
+    {:noreply, %{state | alarm: new_alarm_time}}
   end
 
   def handle_info(:working_working, %{ui_pid: ui, time: time, alarm: alarm, st: Working} = state) do
